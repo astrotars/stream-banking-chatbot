25ce4ed070c8abc7e89548506e107ad771e1982a
Jeff Taggart <jeff@jetaggart.com>
2019-11-03 11:49:08 -0700

    successful money movement

--- backend/src/controllers/v1/message.js
+++ backend/src/controllers/v1/message.js
@@ -3,9 +3,11 @@ import { StreamChat } from 'stream-chat';
 import { generator } from './virgil-credentials';
 import { EThree } from '@virgilsecurity/e3kit';
 
+const { pb } = require('pb-util');
+
 const dialogflow = require('dialogflow');
 const uuid = require('uuid');
-
+const balances = {};
 
 dotenv.config();
 
@@ -13,8 +15,14 @@ exports.message = async (req, res) => {
   try {
     const data = req.body;
     console.log(data);
-    const userId = data["user"]["id"];
-    if (data["type"] === "message.new" && userId !== 'chatbot') {
+    const userId = data['user']['id'];
+    if (data['type'] === 'message.new' && userId !== 'chatbot') {
+      if (!balances[userId]) {
+        balances[userId] = {
+          checking: 1000,
+          savings: 500,
+        };
+      }
       const apiKey = process.env.STREAM_API_KEY;
       const apiSecret = process.env.STREAM_API_SECRET;
 
@@ -23,14 +31,15 @@ exports.message = async (req, res) => {
 
       const chatbotToken = generator.generateToken('chatbot');
       const eThree = await EThree.initialize(() => chatbotToken);
-      const publicKeys = await eThree.lookupPublicKeys([userId]);
+      const publicKey = (await eThree.lookupPublicKeys([userId]))[userId];
+      const userMessage = await eThree.decrypt(data['message']['text'], publicKey);
 
       // A unique identifier for the given session
       const sessionId = uuid.v4();
 
       // Create a new session
       const sessionClient = new dialogflow.SessionsClient();
-      const sessionPath = sessionClient.sessionPath('test-udklyf', sessionId);
+      const sessionPath = sessionClient.sessionPath(process.env.GOOGLE_APPLICATION_PROJECT_ID, sessionId);
 
       // The text query request.
       const request = {
@@ -38,7 +47,7 @@ exports.message = async (req, res) => {
         queryInput: {
           text: {
             // The query to send to the dialogflow agent
-            text: 'hello',
+            text: userMessage,
             // The language used by the client (en-US)
             languageCode: 'en-US',
           },
@@ -48,22 +57,35 @@ exports.message = async (req, res) => {
       // Send request and log result
       const responses = await sessionClient.detectIntent(request);
       const result = responses[0].queryResult;
-      console.log("Intent", result.intent);
+      console.log("Result", result);
       let text = '';
-      if (result.intent) {
-        text = `Intent: ${result.intent.displayName}`;
+      if (result.intent.displayName === 'Check Accounts') {
+        text = `Here are your balances\nChecking: $${balances[userId].checking}\nSavings: $${balances[userId].savings}`;
+      } else if (result.intent.displayName === 'Transfer Money') {
+        const parameters = pb.decode(result.parameters);
+        console.log('parameters', parameters);
+        const transfer = parameters.transfer;
+        const amount = parameters.amount.amount;
+        if (transfer === 'checking to savings') {
+          balances[userId].checking -= amount;
+          balances[userId].savings += amount;
+          text = result.fulfillmentText;
+        } else if (transfer === 'savings to checking') {
+          balances[userId].checking += amount;
+          balances[userId].savings -= amount;
+          text = result.fulfillmentText;
+        } else {
+          text = 'Failed to transfer, unknown accounts';
+        }
       } else {
-        text = "I didn't understand that";
+        text = result.fulfillmentText;
       }
 
-      const encryptedText = await eThree.encrypt(text, publicKeys[userId]);
+      const encryptedText = await eThree.encrypt(text, publicKey);
       const message = {
         text: encryptedText,
         user: { id: 'chatbot' },
       };
-      console.log('text', text);
-      console.log('publicKeys', publicKeys);
-      console.log('encryptedText', encryptedText);
 
       await channel.sendMessage(message);
     }
